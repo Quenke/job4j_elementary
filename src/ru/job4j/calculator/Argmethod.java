ea21dfd187447a3bca8bfe7dcc34d4272ee498f3
Quenke <1bigstepforward1@gmail.com>
2020-11-01 15:41:06 +0300

    Аргументы [#397357]

---
+++ src/ru/job4j/calculator/Argmethod.java
@@ -0,0 +1,17 @@
+package ru.job4j.calculator;
+
+public class Argmethod {
+    public static void hello(String name) {
+        System.out.println("Hello, " + name);
+    }
+
+    public static void main(String[] args) {
+        String name = "Petr Arsentev";
+        int age = 33;
+        System.out.println(age);
+        Argmethod.hello(name);
+        Argmethod.hello(name);
+        Argmethod.hello(name);
+        Argmethod.hello(name);
+    }
+}
