b70ccc7fa108397a4eccc7a3f1a560abba9fc2ca
Quenke <1bigstepforward1@gmail.com>
2020-11-01 14:14:50 +0300

    Разбор первой программы. [#397366]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Artem Shitov");
+        System.out.println("03.09.1993");
+    }
+}
