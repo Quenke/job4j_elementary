b35b6f6a464a9aea006c7a83e9470c470a81d301
Quenke <1bigstepforward1@gmail.com>
2020-11-11 19:06:16 +0300

    String

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,16 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "i like Java";
+        String separate1 = "   ";
+        String separate = " - ";
+        System.out.println(idea + separate1 + "But I am a newbie");
+        int year = 2020;
+
+        System.out.println (idea + separate + year);
+
+
+
+    }
+}
