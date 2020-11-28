ed72b0f4e7be5932fabcecd049b4261e753230a8
Quenke <1bigstepforward1@gmail.com>
2020-11-28 20:48:27 +0300

    Вложенный оператор if else

---
+++ src/ru/job4j/condition/MultiMax.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+public class MultiMax {
+    public static int max(int first, int second, int third) {
+        int result = 0;
+        if (first > second && first > second) {
+            result = first;
+        }
+        else if (second > third) {
+            result = second;
+        }
+        else result = third;
+        return result;
+    }
+}
---
+++ test/ru/job4j/condition/MultiMaxTest.java
@@ -0,0 +1,28 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class MultiMaxTest {
+    @Test
+    public void whenSecondMax() {
+        int result = MultiMax.max(1, 4, 2);
+        assertThat(result, is(4));
+    }
+    @Test
+    public void whenfirstMax() {
+        int result = MultiMax.max(5, 4, 2);
+        assertThat(result, is(5));
+    }
+    @Test
+    public void whenthirdMax() {
+        int result = MultiMax.max(1, 4, 8);
+        assertThat(result, is(8));
+    }
+    @Test
+    public void wheneq() {
+        int result = MultiMax.max(1, 1, 1);
+        assertThat(result, is(1));
+    }
+}
