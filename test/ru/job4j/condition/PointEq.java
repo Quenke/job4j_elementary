1a43a1c9b7eb77b317b4b162deee40246b35d844
Quenke <1bigstepforward1@gmail.com>
2020-11-28 12:11:38 +0300

    if c return

--- test/ru/job4j/condition/PointEq.java
+++ test/ru/job4j/condition/PointEq.java
@@ -4,7 +4,7 @@ public class PointEq {
     public static boolean eq(int x1, int y1, int x2, int y2) {
         boolean eqX = x1 == x2;
         boolean eqY = y1 == y2;
-        return eqX;
+        return eqX && eqY;
     }
 
 
