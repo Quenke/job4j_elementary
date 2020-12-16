d8cc713199005bdcbb59c2520955694bf56a22fa
Quenke <1bigstepforward1@gmail.com>
2020-12-16 10:08:15 +0300

    Заполнение массива

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -8,6 +8,15 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Artem Shitov";
+        names[1] = "Ivan Ivanov";
+        names[2] = "Sergey Sergeyev";
+        names[3] = "Anna Pavlova";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
 
     }
 }
