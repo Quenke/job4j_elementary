6ab3d96a40f2c8b0b45739e437197e460dc8d9ed
Quenke <1bigstepforward1@gmail.com>
2020-12-25 14:51:18 +0300

    Слово начинается с

--- src/ru/job4j/array/ArrayChar.java
+++ src/ru/job4j/array/ArrayChar.java
@@ -2,14 +2,14 @@ package ru.job4j.array;
 
 public class ArrayChar {
     public static boolean startsWith(char[] word, char[] pref) {
-        boolean result = false;
-        for (int index = 0; index < word.length - 1; index++) {
-            for (int index1 = 0; index1 < pref.length - 1; index1++) {
-                if (word[index + 1] == pref[index1 + 1]) {
-                    result = true;
+        boolean result = true;
+        for (int index = 0; index < pref.length; index++) {
+                if (word[index] != pref[index]) {
+                    result = false;
+                    break;
                 }
             }
-        }
+
         return result;
     }
 }
