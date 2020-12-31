e48479fa64c81553f13e84d8d4ea7ae5bf260224
Quenke <1bigstepforward1@gmail.com>
2020-12-31 13:19:42 +0300

    6.4.2. Слово заканчивается на

--- src/ru/job4j/array/EndsWith.java
+++ src/ru/job4j/array/EndsWith.java
@@ -4,7 +4,7 @@ public class EndsWith {
     public static boolean endsWith(char[] word, char[] post) {
         boolean result = true;
         for (int index = post.length - 1; index >= 0; index--) {
-            if (word[index] != post[index]) {
+            if (word[word.length - index - 1] != post[post.length - index - 1]) {
                 result = false;
                 break;
             }
